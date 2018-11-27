254fc0d8c4a39323646dcf596c05c16a4e83d455
3553x <3553x@tuta.io>
2018-11-27 17:09:58 +0000

    Disable debugging mode, fix nasty bug and remove test

--- arduino_inter.py
+++ arduino_inter.py
@@ -1,21 +1,18 @@
-#!/usr/bin/python3
-import serial, sys
-
-from Crypto.Cipher import AES
+import serial, time
 
 BAUD_RATE = 2_000_000
-DEBUG = 1
+DEBUG = 0
 BS = 16
 KEY = "c1e5ec7b1a30e0da98d34ff07030fe65"
 
 class ArdSerial:
 
-    def __init__ (self, dev="/dev/ttyACM0"):
+    def __init__ (self, dev="/dev/ttyACM1"):
         self.serial = serial.Serial(dev, BAUD_RATE)
+        time.sleep(2) #Write is missed otherwise
 
 
     def encrypt(self, cleartext):
-        print(cleartext) 
         assert len(cleartext) == BS
 
         if(DEBUG):
@@ -26,31 +23,13 @@ class ArdSerial:
                 if(line == b"NEED INPUT\r\n"):
                     break
 
-        self.serial.write(cleartext)
-        self.serial.flush()
+        assert BS == self.serial.write(cleartext)
 
         if(DEBUG):
             print(self.serial.readline())
             rec_pt = self.serial.read(BS)
             print(rec_pt.hex())
 
-
         ct = self.serial.read(BS)
         
-        if(DEBUG):
-            print(self.serial.readline())
         return ct
-
-
-
-if(__name__ == "__main__"):
-    inter = ArdSerial()
-    ct = inter.encrypt(bytes([0]*16))
-
-    cipher = AES.new(bytes(bytearray.fromhex(KEY)), AES.MODE_ECB)
-    ct2 = cipher.encrypt(bytes([0]*16))
-
-    print(ct.hex())
-    print(ct2.hex())
-    
-
