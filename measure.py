faea54bc22dc11fc7435c87d87b5ee4ca26a60e3
3553x <3553x@tuta.io>
2018-11-29 19:49:22 +0000

    Improve performence of measurement script, now with 4 measurements per second

--- measure.py
+++ measure.py
@@ -5,36 +5,26 @@ from converter.tektronix_converter import convert
 
 from Crypto.Cipher import AES
 from os import urandom
-from sqlalchemy import create_engine
-from threading import Thread
-
-import pandas
-import io
 
 def init_scope(scope):
-  scope.acquire_stopafter("SEQ")
+  scope.acquire_stopafter("RUNST")
   scope.data_encoding("RIB")
   scope.data_width("2")
   scope.data_start("1")
   scope.data_stop("10000")
-  scope.acquire_state("OFF")
+  scope.acquire_state("RUN")
+  while("ARMED" in scope.trigger_state()):
+    pass
 
 def take_measurement(scope, arduino, cleartext):
-  scope.acquire_state("RUN")
-  while(scope.trigger_state() == "ARMED"):
-    pass
   ciphertext = arduino.encrypt(cleartext)
-  while(scope.busy() == "1"):
+  while(scope.busy() != ":BUSY 0"):
     pass
     
-  scope.data_source("CH1")
-  csv_ch1 = convert(scope.waveform())
-  scope.data_source("CH2")
-  csv_ch2 = convert(scope.waveform())
-  scope.data_source("CH3")
-  csv_ch3 = convert(scope.waveform())
+  scope.data_source("MATH")
+  csv = convert(scope.waveform())
 
-  return (ciphertext, csv_ch1, csv_ch2, csv_ch3)
+  return (ciphertext, csv)
 
 
 def gather_data(n):
@@ -45,24 +35,21 @@ def gather_data(n):
 
   arduino = ArdSerial()
 
-  engine = create_engine("sqlite:///measurements.db")
-  con = engine.connect()
-  con.execute("CREATE TABLE IF NOT EXISTS data (ciphertext TEXT NOT NULL, plaintext TEXT NOT NULL);")
+  capt_f = open("captures", 'a')
+  pt_f = open("plaintext", "ab")
+  ct_f = open("ciphertext", "ab")
+
 
   for x in range(n):
     plaintext = urandom(BS)
-    (ciphertext, csv_ch1, csv_ch2, csv_ch3) = take_measurement(scope, arduino, plaintext)
+    (ciphertext, csv) = take_measurement(scope, arduino, plaintext)
     assert ciphertext == crypt.encrypt(plaintext)
-    df_1 = pandas.read_csv(io.StringIO(csv_ch1), names=["S", "V"])
-    df_2 = pandas.read_csv(io.StringIO(csv_ch2), names=["S", "V"])
-    df_3 = pandas.read_csv(io.StringIO(csv_ch3), names=["S", "V"])
-    df_1.to_sql('samples_ch1', engine, if_exists='append', index=False)
-    df_2.to_sql('samples_ch2', engine, if_exists='append', index=False)
-    df_3.to_sql('samples_ch3', engine, if_exists='append', index=False)
+    
+    capt_f.write(csv)
+    pt_f.write(plaintext)
+    ct_f.write(ciphertext)
+ 
 
-    con.execute("INSERT INTO data VALUES ('%s', '%s')" % (ciphertext.hex(), plaintext.hex()))
-
-  con.close()
 
 if(__name__ == "__main__"):
-  gather_data(5)
+  gather_data(100)
